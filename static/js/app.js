939d5675ce59a3dff3875be09bc87341a7dc4347
Jacqueline Meyer <jacquelinemeye1000@gmail.com>
2021-04-15 19:51:51 -0500

    added reset button 'full table'

--- static/js/app.js
+++ static/js/app.js
@@ -3,6 +3,7 @@ var tableData = data;
 
 // Get a reference to the table body
 var tbody = d3.select("tbody");
+var full_btn = d3.select("#full-btn");
 
 // Console.log the UFO data from data.js
 console.log(data);
@@ -45,21 +46,7 @@ d3.selectAll("#filter-btn").on("click", handleClick);
 // Build Table with data.js 
 buildTable(tableData);
 
-// Level 2: ----------------------------------------------------
-// Adjust for queries to understand other filter searches
-citysubmitButton.on("click", function() {
-    // clears the data of the current table        
-    tbody.html("");
-    // stop the page from refresh
-    d3.event.preventDefault();
-    // print "You have just clicked the 'Filter Table' on console, for testing
-    console.log("You have just clicked the 'City Filter Button'.");
-    // get the value property of the "input" element 
-    var cityinputElement = cityinputField.property("value");
-    // print value in console
-    console.log(cityinputElement);
-    // use the "field input" to filter the data by "date" only
-    var cityinputTypeArray = data.filter(two => two.city === cityinputElement);   
-    console.log(cityinputTypeArray)
-});
-
+// Select the Full Table Button
+full_btn.on("click", function() {
+    update_table(tableData);
+});  
